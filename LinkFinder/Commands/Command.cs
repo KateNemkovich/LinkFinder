8f3da1a1ffe0a2e87dc1275ca16af2d7fae7fc80
Kate <nemkovich.kate@gmail.com>
2022-10-27 23:27:19 +0300

    With new filter

--- LinkFinder/Commands/Command.cs
+++ LinkFinder/Commands/Command.cs
@@ -1,6 +1,7 @@
 ﻿using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
 using Nice3point.Revit.Toolkit.External;
 using LinkFinder.ViewModels;
 using LinkFinder.Views;
@@ -14,12 +15,20 @@ public class Command : ExternalCommand
     public override void Execute()
     {
        //Получаем список документов и из них выбираем один который связанный
-        var documentLink = Application.Documents.Cast<Document>().First(document => document.IsLinked);
-        var doors = new FilteredElementCollector(documentLink,ActiveView.Id)
-            .WhereElementIsNotElementType()
-            .OfCategory(BuiltInCategory.OST_Doors)
-            .ToElements();
+        var documentLink = Application.Documents.Cast<Document>().Where(document => document.IsLinked);
+        var count=0;
+        foreach (var document in documentLink)
+        {
+            var doors = new FilteredElementCollector(document)
+                .WhereElementIsNotElementType()
+                .OfCategory(BuiltInCategory.OST_Doors)
+                .WherePasses( new SelectableInViewFilter(Document,Document.ActiveView.Id))
+                .ToElements();
+            count =count+doors.Count;
+        }
+       
     }
+   
 }
     
       
