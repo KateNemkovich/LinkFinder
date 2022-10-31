7cba6e262ae11e58d3c683eb422955e90baa458f
Kate <nemkovich.kate@gmail.com>
2022-10-31 23:27:32 +0300

    Version with document

--- LinkFinder/Commands/Command.cs
+++ LinkFinder/Commands/Command.cs
@@ -14,46 +14,43 @@ public class Command : ExternalCommand
 {
     public override void Execute()
     {
-       //Получаем список документов и из них выбираем один который связанный
-        var documentLink = Application.Documents.Cast<Document>().Where(document => document.IsLinked);
-        var count=0;
-
-        var boundingBox = ActiveView.get_BoundingBox(null);
-        var viewPlan = ((ViewPlan) ActiveView);
-        var viewRange = viewPlan.GetViewRange();
-        var minOffset = viewRange.GetOffset(PlanViewPlane.BottomClipPlane);
-        var minlevelId = viewRange.GetLevelId(PlanViewPlane.BottomClipPlane);
-        var maxOffset = viewRange.GetOffset(PlanViewPlane.CutPlane);
-        var maxLevelId = viewRange.GetLevelId(PlanViewPlane.CutPlane);
-        var maxLevel = ((Level) maxLevelId.ToElement(Document)).ProjectElevation;
-        var maxLevelZ = maxLevel+maxOffset;
+             var viewPlan = ((ViewPlan) ActiveView);
+            var viewRange = viewPlan.GetViewRange();
+            
+            var minOffset = viewRange.GetOffset(PlanViewPlane.BottomClipPlane);
+            var maxOffset = viewRange.GetOffset(PlanViewPlane.CutPlane);
+            
+            var minLevelId = viewRange.GetLevelId(PlanViewPlane.BottomClipPlane);
+            var maxLevelId = viewRange.GetLevelId(PlanViewPlane.CutPlane);
+            
+            var maxLevel = ((Level) maxLevelId.ToElement(Document))!.ProjectElevation;
+            var maxLevelZ = maxLevel+maxOffset;
+            var minLevelZ= minLevelId == ElementId.InvalidElementId
+                ? double.MinValue
+                :((Level) minLevelId.ToElement(Document))!.ProjectElevation + minOffset;
         
-        double minLevelZ;
-        if (minlevelId == ElementId.InvalidElementId)
-        {
-            minLevelZ= double.MinValue;
-        }
-        else
-        {
-           var level = ((Level) minlevelId.ToElement(Document)).ProjectElevation;
-           minLevelZ = level + minOffset;
-        }
-        
-        new XYZ(boundingBox.Max.X, boundingBox.Max.Y, minLevelZ);
-        new XYZ(boundingBox.Min.X, boundingBox.Min.Y, maxLevelZ);
-          
-        foreach (var document in documentLink)
-        {
-            var doors = new FilteredElementCollector(document)
-                .WhereElementIsNotElementType()
-                .OfCategory(BuiltInCategory.OST_Doors)
-                .WherePasses( new BoundingBoxIntersectsFilter(new Outline()))
-                .ToElements();
-            count =count+doors.Count;
-        }
-       
+            var boundingBox = ActiveView.get_BoundingBox(null);
+            var maximumPoint = new XYZ(boundingBox.Max.X, boundingBox.Max.Y, maxLevelZ);
+            var minimumPoint = new XYZ(boundingBox.Min.X, boundingBox.Min.Y, minLevelZ);
+            
+            var outline = new Outline(minimumPoint, maximumPoint);
+            var boundingBoxIntersectsFilter = new BoundingBoxIntersectsFilter(outline); 
+            
+            //Получаем список документов и из них выбираем один который связанный
+            var documentLink = Application.Documents.Cast<Document>().Where(document => document.IsLinked);
+            var count=0;
+            
+            foreach (var document in documentLink)
+            {
+                var doors = new FilteredElementCollector(document)
+                    .WhereElementIsNotElementType()
+                    .OfCategory(BuiltInCategory.OST_Doors)
+                    .WherePasses(boundingBoxIntersectsFilter)
+                    .ToElements();
+                count+=doors.Count;
+            }
     }
-   
 }
+
     
       
